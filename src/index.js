414c3f0cc0ed88326d67affb8f7b2b77c0eeb977
Sontara444 <sontararajput@gmail.com>
2026-05-09 21:22:09 +0530

    setup project

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+require('./config/db'); // Test DB connection
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('School Management API is running.');
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
