b16b880c918e4c60037cc11528b4e2207a4ff384
Sontara444 <sontararajput@gmail.com>
2026-05-11 00:30:15 +0530

    db error fix

--- src/config/db.js
+++ src/config/db.js
@@ -6,12 +6,16 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-// Test the connection
+// Test connection
 pool.getConnection()
     .then(connection => {
         console.log('Successfully connected to the database.');
